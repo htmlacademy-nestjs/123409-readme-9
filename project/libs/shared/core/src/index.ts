e46d78647443e7a0cec959fca26997b6ed6a361e
Aleksander <Ivanovaa1992@gmail.com>
2025-05-28 21:18:20 +0300

    add storable entity interface

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -2,3 +2,6 @@ export { Entity } from './lib/base/entity';
 
 export type { User } from './lib/types/user/user.interface';
 export type { AuthUser } from './lib/types/user/auth-user.interface';
+
+export type { StorableEntity } from './lib/interfaces/storable-entity.interface';
+export type { EntityFactory } from './lib/interfaces/entity-factory.interface';
