92a00b686f089604b9066a7740ee04acbb8aab59
Aleksander <Ivanovaa1992@gmail.com>
2025-05-28 18:47:26 +0300

    reexport from lib

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -1,1 +1,4 @@
-export * from './lib/core.js';
+export { Entity } from './lib/base/entity';
+
+export type { User } from './lib/types/user/user.interface';
+export type { AuthUser } from './lib/types/user/auth-user.interface';
