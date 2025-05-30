b4250f09b4e278e8750b931961610c70025d59e4
Aleksander <Ivanovaa1992@gmail.com>
2025-05-30 19:32:23 +0300

    feat: connect post and post comment modules to blog module

--- project/libs/blog/post/src/post-comment-module/post-comment.module.ts
+++ project/libs/blog/post/src/post-comment-module/post-comment.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostCommentFactory } from './post-comment.factory';
 import { PostCommentRepository } from './post-comment.repository';
+import { PostCommentController } from './post-comment.controller';
 
 @Module({
     providers: [PostCommentFactory, PostCommentRepository],
+    controllers: [PostCommentController],
     exports: [PostCommentRepository],
 })
 export class PostCommentModule {} 
