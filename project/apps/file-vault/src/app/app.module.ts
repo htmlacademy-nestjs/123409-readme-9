c50bf2e96b7e73aceada1dcde33530b54b548346
Aleksander <Ivanovaa1992@gmail.com>
2025-06-21 18:58:14 +0300

    file uploader

---
+++ project/apps/file-vault/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { FileUploaderModule } from '@project/file-uploader';
+import { FileVaultConfigModule } from '@project/file-vault-config';
+
+@Module({
+  imports: [
+    FileVaultConfigModule,
+    FileUploaderModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+
+export class AppModule {}
